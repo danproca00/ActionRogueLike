1ed92515e63acfb99372d939edd1918f2540873e
Dan Proca <dan.proca00@e-uvt.ro>
2024-09-15 18:09:51 +0300

    Environment Query Spawn Logic
    More AI, Environment Query Spawn Logic

---
+++ Source/ActionRogueLike/Private/SGameModeBase.cpp
@@ -0,0 +1,81 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SGameModeBase.h"
+#include "EnvironmentQuery/EnvQueryManager.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+#include "EnvironmentQuery/EnvQueryInstanceBlueprintWrapper.h"
+#include "AI/SAICharacter.h"
+#include "SAttributeComponent.h"
+#include "EngineUtils.h"
+
+
+ASGameModeBase::ASGameModeBase()
+{
+	SpawnTimerInterval = 2.0f;
+}
+
+void ASGameModeBase::StartPlay()
+{
+	//without super - none of the actors would recieve the begin play
+	Super::StartPlay();
+
+	//set up a timer
+	GetWorldTimerManager().SetTimer(TimerHandle_SpawnBots, this, &ASGameModeBase::SpawnTimerElapsed, SpawnTimerInterval, true);
+}
+
+
+void ASGameModeBase::SpawnTimerElapsed()
+{
+	//where we re doing the EQS query
+	UEnvQueryInstanceBlueprintWrapper* QueryInstance =  UEnvQueryManager::RunEQSQuery(this, SpawnBotQuery, this, EEnvQueryRunMode::RandomBest5Pct, nullptr);
+	if (ensure(QueryInstance)) 
+	{
+		QueryInstance->GetOnQueryFinishedEvent().AddDynamic(this, &ASGameModeBase::OnQueryCompleted);
+	}
+}
+
+//called a few frames after we have triggered our run EQS query => give us result/location where we want to spawn our bots
+void ASGameModeBase::OnQueryCompleted(UEnvQueryInstanceBlueprintWrapper* QueryInstance, EEnvQueryStatus::Type QueryStatus /*fail/success*/)
+{
+	if (QueryStatus != EEnvQueryStatus::Success)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Spawn bot EQS query failed :("));
+		return;
+	}
+
+	//a limit to not spawn infinite ai bots
+	int32 NrOfAliveBots = 0;
+	for (TActorIterator<ASAICharacter> It(GetWorld()); It; ++It)
+	{
+		ASAICharacter* Bot = *It;
+
+		USAttributeComponent* AttributeComp = Cast<USAttributeComponent>(Bot->GetComponentByClass(USAttributeComponent::StaticClass()));
+		if (ensure(AttributeComp) && AttributeComp->IsAlive())
+		{
+			NrOfAliveBots++;
+		}
+	}
+	
+	//the maximum of bots
+	float MaxBotCount = 10.0f;
+
+	if (DifficultyCurve)
+	{
+		MaxBotCount = DifficultyCurve->GetFloatValue(GetWorld()->TimeSeconds /*time increseas => more bots*/);
+	}
+	if (NrOfAliveBots >= MaxBotCount)
+	{
+		return;
+	}
+
+	//we have to make sure that is not a null pointer
+	
+
+	TArray<FVector> Locations = QueryInstance->GetResultsAsLocations();
+
+	if (Locations.IsValidIndex(0))
+	{
+		GetWorld()->SpawnActor<AActor>(MinionClass, Locations[0], FRotator::ZeroRotator);
+	}
+}
---
+++ Source/ActionRogueLike/Public/SGameModeBase.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EnvironmentQuery\EnvQueryTypes.h" //enums specified by the system
+#include "SGameModeBase.generated.h"
+
+
+class UEnvQuery;
+class UEnvQueryInstanceBlueprintWrapper;
+class UCurveFloat;
+
+UCLASS()
+class ACTIONROGUELIKE_API ASGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	TSubclassOf<AActor> MinionClass;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	UEnvQuery* SpawnBotQuery;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	UCurveFloat* DifficultyCurve;
+
+	FTimerHandle TimerHandle_SpawnBots;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	float SpawnTimerInterval;
+
+	UFUNCTION()
+	void SpawnTimerElapsed();
+
+	UFUNCTION()
+	void OnQueryCompleted(UEnvQueryInstanceBlueprintWrapper* QueryInstance, EEnvQueryStatus::Type QueryStatus);
+public:
+
+	//constructor to add some variables later
+	ASGameModeBase();
+
+// it s similar to begin play -> calls it on all the actors in the game world
+	virtual void StartPlay() override;
+};
